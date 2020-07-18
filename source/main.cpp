f36027a8847b7e0a62db6f894b57d2f997be55df
Kevin <huaziplszl@gmail.com>
2020-07-18 12:20:15 +0800

    Initial Commit.

---
+++ source/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+TEST(name, c1) {
+    std::cout << "Hello Google Test." << std::endl;
+}
---
+++ tests/demo_test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Kevin on 2020/7/18.
+//
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(test, c1) {
+    cout << "Hello Google Test." << endl;
+}
